aec8cf1708919bd498f79b78accdebf180f5f37e
desappr <Byes1013>
2021-03-30 22:18:30 +0300

    Initial commit

---
+++ src/test/java/com/example/unischedulewebapp/UniScheduleWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unischedulewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniScheduleWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
